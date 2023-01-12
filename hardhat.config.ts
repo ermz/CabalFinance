e976b6dc2537e9a81f66ee7a7784212c92103ce4
ermz <eramirez4369@gmail.com>
2023-01-12 01:31:17 -0500

    Added helper config file and began setup for VRF Coordinator Mock contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-waffle";
+import dotenv from "dotenv";
+import { network } from "hardhat";
+import { networkConfig } from "./helper-hardhat-config"
+
+dotenv.config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.g.alchemy.com/v2/c8LuNzJlVdS8ffj1-e4TRyHRlUEEk13H"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    goerli: {
+      chainId: 5,
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    }
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.8.4",
+      }
+    ]
+  },
 };
 
 export default config;
