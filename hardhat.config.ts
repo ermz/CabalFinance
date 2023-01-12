b001f7399e43ed22b1f124b6d2e280089517c4fd
ermz <eramirez4369@gmail.com>
2023-01-12 13:37:19 -0500

    Fixed hardhat node_module issue, need to fix typechain node_modules

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-waffle";
-import dotenv from "dotenv";
-import { network } from "hardhat";
-import { networkConfig } from "./helper-hardhat-config"
+// import dotenv from "dotenv";
+// import { network } from "hardhat";
+// import { networkConfig } from "./helper-hardhat-config"
 
-dotenv.config();
+// dotenv.config();
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.g.alchemy.com/v2/c8LuNzJlVdS8ffj1-e4TRyHRlUEEk13H"
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+// const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.g.alchemy.com/v2/c8LuNzJlVdS8ffj1-e4TRyHRlUEEk13H"
+// const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-    },
-    goerli: {
-      chainId: 5,
-      url: GOERLI_RPC_URL,
-      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-    }
-  },
+  // defaultNetwork: "hardhat",
+  // networks: {
+  //   hardhat: {
+  //     chainId: 31337,
+  //   },
+  //   goerli: {
+  //     chainId: 5,
+  //     url: GOERLI_RPC_URL,
+  //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+  //   }
+  // },
   solidity: {
     compilers: [
       {
